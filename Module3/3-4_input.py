c6a901c30c9fc368b48dd9e0ba25f5ad80981ad7
RitataHuang <r8012197@gmail.com>
2024-11-15 16:29:59 +0800

    BDSE36_python_lesson_20241115

--- Module3/3-4_input.py
+++ Module3/3-4_input.py
@@ -1,7 +1,13 @@
 # input() 函數 讓使用者在終端機輸入資料
 
-# 取得使用者輸入的資料
 
+# 取得使用者輸入的資料
+num = int(input("請輸入數字"))
+
+print(type(num))
+if num > 20:
+    print("num > 20")
+print(num + 10)
 
 
 # 將使用者輸入強制轉型成 int
